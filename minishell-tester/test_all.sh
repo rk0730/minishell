9dd2a7078bdb8a83a23306d10869e5dcc3386e5e
北尾遼馬 <kitaoryoma@kitaoryoubanoMacBook-Air.local>
2024-10-27 18:25:11 +0900

    test_all whileに修正

--- minishell-tester/test_all.sh
+++ minishell-tester/test_all.sh
@@ -2,10 +2,21 @@ source ./test_utils.sh
 
 start_test
 
-source ./test1.sh
-source ./test2.sh
-source ./test3.sh
-source ./test4.sh
-source ./test5.sh
+# 連番のtestスクリプトを順番にsourceする
+tmp=1  # ループのカウンタ
+while true; do
+	file_name="test${tmp}.sh"
+
+	# ファイルが存在するか確認
+	if [ -f "$file_name" ]; then
+		source "./$file_name"
+	else
+		echo "$file_name does not exist."
+		break
+	fi
+
+	# 次の番号へ
+	tmp=$((tmp + 1))
+done
 
 end_test
