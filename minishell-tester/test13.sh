02cd3e147bfd223fabea665713b5faeae5f2e980
北尾遼馬 <kitaoryoma@kitaoryoubanoMacBook-Air.local>
2024-11-18 17:03:08 +0900

    pre_cmdを確かめるテスター追加

---
+++ minishell-tester/test13.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+
+if [ "${BASH_SOURCE[0]}" == "$0" ]; then
+	source ./test_utils.sh
+	start_test
+fi
+
+# pre_cmdモジュールのテスト
+
+exec_test 'export foo="a   b"' 'echo $foo' 'echo "$foo"' './argv $foo' './argv "$foo"' 'exit'
+exec_test 'export TEST="-n hello"' 'env | grep TEST' 'echo $TEST' 'exit'
+exec_test 'export TEST="| grep test"' 'ls $TEST' 'unset TEST' 'exit'
+exec_test 'export TEST="file1 file2"' 'cat < $TEST' 'unset TEST' 'exit'
+exec_test 'export TEST="file1	file2"' 'cat < $TEST' 'unset TEST' 'exit'
+exec_test 'export TEST="file1 file2"' 'cat < "$TEST" > out' 'unset TEST' 'exit'
+exec_test 'export TEST="out1 > out2"' 'ls > $TEST' 'unset TEST' 'exit'
+
+if [ "${BASH_SOURCE[0]}" == "$0" ]; then
+	end_test
+fi
