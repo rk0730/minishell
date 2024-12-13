0dcdb11285a2e5d030e0128613fbc2aeac5e67fd
北尾遼馬 <kitaoryoma@kitaoryoubanoMacBook-Air.local>
2024-12-13 11:01:20 +0900

    ft_cmdinfo_list norm

--- srcs/pre_cmd/ft_cmdinfo_list.c
+++ srcs/pre_cmd/ft_cmdinfo_list.c
@@ -6,7 +6,7 @@
 /*   By: kitaoryoma <kitaoryoma@student.42.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/25 16:12:39 by rkitao            #+#    #+#             */
-/*   Updated: 2024/12/12 21:53:23 by kitaoryoma       ###   ########.fr       */
+/*   Updated: 2024/12/13 11:00:16 by kitaoryoma       ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -58,7 +58,8 @@ static int	_ft_help2(char **cmds, t_env_info *env_info_p, t_cmd_info *cmd_list)
 	tokens = _ft_gen_tokens(cmds[ft_array_len(cmds) - 1]);
 	if (_ft_is_last_redirect(tokens))
 	{
-		ft_printf_fd(STDERR_FILENO, "syntax error near unexpected token `newline'\n");
+		ft_printf_fd(STDERR_FILENO,
+			"syntax error near unexpected token `newline'\n");
 		ft_free_array(tokens);
 		return (1);
 	}
@@ -71,17 +72,24 @@ t_cmd_info	*ft_cmd_info_list(char **cmds, t_env_info *env_info_p)
 {
 	t_cmd_info	*cmd_list;
 	char		**tokens;
-	int 		i;
+	int			i;
 
 	cmd_list = (t_cmd_info *)malloc(sizeof(t_cmd_info) * ft_array_len(cmds));
 	if (!cmd_list)
 		exit(EXIT_FAILURE);
-	if (_ft_help1(cmds, env_info_p, cmd_list) == 1)
-	{
-		free(cmd_list);
-		return (NULL);
-	}
-	if (_ft_help2(cmds, env_info_p, cmd_list) == 1)
+	// if (_ft_help1(cmds, env_info_p, cmd_list) == 1)
+	// {
+	// 	free(cmd_list);
+	// 	return (NULL);
+	// }
+	// if (_ft_help2(cmds, env_info_p, cmd_list) == 1)
+	// {
+	// 	free(cmd_list);
+	// 	return (NULL);
+	// }
+	// C言語のif文は必ず左からチェックし、左が真で||の場合は右を見ないので、これで上のコメントアウトと同じ処理になるはず
+	if (_ft_help1(cmds, env_info_p, cmd_list) || _ft_help2(cmds, env_info_p,
+			cmd_list))
 	{
 		free(cmd_list);
 		return (NULL);
@@ -91,8 +99,8 @@ t_cmd_info	*ft_cmd_info_list(char **cmds, t_env_info *env_info_p)
 	while (i < ft_array_len(cmds))
 	{
 		tokens = _ft_gen_tokens(cmds[i]);
-		RKITAO("tokens\n");
-		ft_show_all(tokens);
+		// RKITAO("tokens\n");
+		// ft_show_all(tokens);
 		_ft_in_out_fd(tokens, *env_info_p, &(cmd_list[i]), cmd_list[i].fd_in);
 		cmd_list[i].cmd_argv = _ft_gen_cmd_argv(tokens, *env_info_p);
 		ft_free_array(tokens);
