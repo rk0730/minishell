1887559f7522fb600c38cb3917a066cd4770c769
北尾遼馬 <kitaoryoma@kitaoryoubanoMacBook-Air.local>
2024-12-14 18:36:39 +0900

    includes コメント削除

--- includes/cmd.h
+++ includes/cmd.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   cmd.h                                              :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: yyamasak <yyamasak@student.42.fr>          +#+  +:+       +#+        */
+/*   By: kitaoryoma <kitaoryoma@student.42.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/07 17:55:05 by rkitao            #+#    #+#             */
-/*   Updated: 2024/12/14 15:41:40 by yyamasak         ###   ########.fr       */
+/*   Updated: 2024/12/14 18:30:20 by kitaoryoma       ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,17 +15,20 @@
 
 # include "builtins.h"
 
-int			ft_exec_cmdline(t_env_info *env_info_p);
-int			ft_exec_cmd(t_cmd_info cmd_info, t_env_info *env_info_p, int read_pipe, int write_pipe);
-int			ft_exec_cmd_list(t_cmd_info *cmd_list, t_env_info *env_info_p, int last_index);
-void		ft_sigint_cmd(int sig);
-void		ft_sigquit_cmd(int sig);
-void		ft_change_g_signum(int sig);
-void		ft_exec_cmd_child(t_cmd_info cmd_info, t_env_info *env_info_p, int read_pipe, int write_pipe);
-int         _ft_wait_pipe(pid_t pid);
-void        _ft_print_newline(int sig);
-void        _ft_close_fd_inout(t_cmd_info *cmd_list, int index);
-int         _ft_exec_one_cmd(t_cmd_info *cmd_list, t_env_info *env_info_p);
-void        _ft_wait_signal(int status);
+int		ft_exec_cmdline(t_env_info *env_info_p);
+int		ft_exec_cmd(t_cmd_info cmd_info, t_env_info *env_info_p, int read_pipe,
+			int write_pipe);
+int		ft_exec_cmd_list(t_cmd_info *cmd_list, t_env_info *env_info_p,
+			int last_index);
+void	ft_sigint_cmd(int sig);
+void	ft_sigquit_cmd(int sig);
+void	ft_change_g_signum(int sig);
+void	ft_exec_cmd_child(t_cmd_info cmd_info, t_env_info *env_info_p,
+			int read_pipe, int write_pipe);
+int		_ft_wait_pipe(pid_t pid);
+void	_ft_print_newline(int sig);
+void	_ft_close_fd_inout(t_cmd_info *cmd_list, int index);
+int		_ft_exec_one_cmd(t_cmd_info *cmd_list, t_env_info *env_info_p);
+void	_ft_wait_signal(int status);
 
 #endif
--- includes/env.h
+++ includes/env.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   env.h                                              :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: yyamasak <yyamasak@student.42.fr>          +#+  +:+       +#+        */
+/*   By: kitaoryoma <kitaoryoma@student.42.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/07 17:04:09 by rkitao            #+#    #+#             */
-/*   Updated: 2024/12/12 16:40:55 by yyamasak         ###   ########.fr       */
+/*   Updated: 2024/12/14 18:29:57 by kitaoryoma       ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,36 +20,32 @@ typedef struct s_env_pair
 	char				*key;
 	char				*value;
 	struct s_env_pair	*next;
-}	t_env_pair;
+}						t_env_pair;
 
-/* 
-last_status: statusはcmdのint *今は使用していない
-			ft_status_code(int flag, int new_status)で管理する
-input: ヒアドクなども含めた入力　環境変数ではないが都合がいいのでここに入れる
-env_list: リスト
-*/
 typedef struct s_env_info
 {
-	t_env_pair	*env_list;
-	char		*input;//プロンプト表示時のユーザーの入力コマンド　環境変数ではないが都合がいいのでここに入れる
-}	t_env_info;
+	t_env_pair			*env_list;
+	char				*input;
+}						t_env_info;
 
-enum e_export_mode
+enum					e_export_mode
 {
 	REPLACE = 0,
 	ADD = 1
 };
 
-t_env_pair	*ft_gen_env_list(char **envp);
-char		**ft_gen_path_array(t_env_pair *env_list);
-void		ft_free_env_list(t_env_pair *env_list);
-void		ft_show_env_list(t_env_pair *env_list);
-char		*ft_search_env(char *search, t_env_pair *env_list);
-t_env_pair	*ft_search_env_node(char *search, t_env_pair *env_list);
-char		**ft_gen_cmd_env(t_env_pair *env_list);
-void		ft_add_env_list(t_env_pair **env_list_p, t_env_pair *new);
-void		ft_update_env_list(t_env_pair **env_list_p, t_env_pair *new, int mode);
-void		ft_show_env_list(t_env_pair *env_list);
-void		ft_show_env_list2(t_env_pair *env_list);
+t_env_pair				*ft_gen_env_list(char **envp);
+char					**ft_gen_path_array(t_env_pair *env_list);
+void					ft_free_env_list(t_env_pair *env_list);
+void					ft_show_env_list(t_env_pair *env_list);
+char					*ft_search_env(char *search, t_env_pair *env_list);
+t_env_pair				*ft_search_env_node(char *search, t_env_pair *env_list);
+char					**ft_gen_cmd_env(t_env_pair *env_list);
+void					ft_add_env_list(t_env_pair **env_list_p,
+							t_env_pair *new);
+void					ft_update_env_list(t_env_pair **env_list_p,
+							t_env_pair *new, int mode);
+void					ft_show_env_list(t_env_pair *env_list);
+void					ft_show_env_list2(t_env_pair *env_list);
 
 #endif
